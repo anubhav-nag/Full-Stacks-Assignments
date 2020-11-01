04fc310120fa1623c785e9fe50110356cf36d704
anubhav-nag <anubhav.nag_cs18@gla.ac.in>
2020-11-01 23:24:24 +0530

    Assignment-15 done

---
+++ Javascript part/Assignment-15.js
@@ -0,0 +1,32 @@
+let b1 = document.getElementById("btn1");
+let display = document.getElementById("counter");
+let b2 = document.getElementById("btn2");
+
+let count = 0;
+
+display.textContent = count;
+
+b2.addEventListener("click",increase);
+function increase(){
+    if(count >= 0){
+        b1.disabled = false;
+    }
+    if(count <10){
+    count++;
+    display.textContent = count;
+    }
+    if(count = 10){
+        b2.disabled = true;
+    }
+}
+
+b1.addEventListener("click",decrease);
+function decrease(){
+    if(count <= 1){
+        b1.disabled = true;
+    }
+    if(count >= 1){
+        count--;
+        display.textContent = count;
+    }
+}
