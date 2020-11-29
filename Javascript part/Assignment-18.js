59138ddba45b7dc963f49e1ad1fea157c727f86b
anubhav-nag <anubhav.nag_cs18@gla.ac.in>
2020-11-29 22:05:29 +0530

    Halfly completd javascript for assignment-18

---
+++ Javascript part/Assignment-18.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const small = document.getElementById('small');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    checkInput();
+});
+
+function checkInput(){
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const password1Value = password1.value;
+    const password2Value = password2.value;
+
+    if(usernameValue ===''){
+        showError(username, "Username can not be blank");
+    }
+    else{
+        showSuccess(username);
+    }
+
+
+}
+
+function showError(input,msg){
+    const formControl = input.parentNode;
+    formControl.className = 'form-container error';
+    const small = formControl.querySelector('small');
+    small.innerHTML = msg;
+}
+
+function showSuccess(input){
+    const formControl = input.parentNode;
+    formControl.className = 'form-container success';
+}
+
